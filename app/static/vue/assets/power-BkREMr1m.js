f69d51dd9fe7661487a425a62f58cf4be3db52ab
DEHER Rémi <remi2r@hotmail.fr>
2026-07-20 09:58:23 +0200

    Corrige la largeur du layout sur mobile

--- app/static/vue/assets/power-BkREMr1m.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-CMFJ9QQW.js";const e=[["path",{d:"m10 17 5-5-5-5",key:"1bsop3"}],["path",{d:"M15 12H3",key:"6jk70r"}],["path",{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4",key:"u53s6r"}]],c=o("log-in",e);const a=[["path",{d:"M12 2v10",key:"mnfbl"}],["path",{d:"M18.4 6.6a9 9 0 1 1-12.77.04",key:"obofu9"}]],n=o("power",a);export{c as L,n as P};
